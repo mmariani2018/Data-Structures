85cd2959587ebf1f4dffe165070f2c612981e238
mmariani2018 <mmariani2018@students.curry.edu>
2021-09-17 17:05:39 -0400

    initial commit

---
+++ Mariani.App2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Mariani.App2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new App2Design.CreateArray();
+        }
+    }
+}
